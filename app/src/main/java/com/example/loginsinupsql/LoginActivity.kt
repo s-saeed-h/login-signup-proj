25394e0ba093e2853def77ec68dc3c0519260117
s.saeed <saeed.rodbar5@gmail.com>
2024-12-27 13:32:31 +0330

    login signup proj

---
+++ app/src/main/java/com/example/loginsinupsql/LoginActivity.kt
@@ -0,0 +1,46 @@
+package com.example.loginsinupsql
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginsinupsql.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+        binding.loginButton.setOnClickListener{
+        val loginUsername =binding.loginUsername.text.toString()
+        val loginPassword = binding.loginPassword.text.toString()
+        loginDatabase(loginUsername,loginPassword)
+        }
+        binding.signupRedirect.setOnClickListener{
+            val intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+    private fun loginDatabase(username:String, password : String){
+        val userExists = databaseHelper.readUser(username , password)
+        if(userExists){
+            Toast.makeText(this,"Login successful",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+        else{
+            Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
